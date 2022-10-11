bb1c37b622041ef47d2b8fc44fd5a7568fd06493
Ismael Bueno <ismaelwno@gmail.com>
2022-10-12 00:31:22 +0200

    Implemented forward method

--- runge-kutta/src/rk_integrator.cpp
+++ runge-kutta/src/rk_integrator.cpp
@@ -22,27 +22,27 @@ namespace rk
                                                const vector1d &vars,
                                                vector1d (*ode)(double, const vector1d &)) const
     {
-        vector2d k_vectors = reserve(m_tableau.stage(), vars.size());
+        vector2d kvec = reserve(m_tableau.stage(), vars.size());
         vector1d vars_aux;
         vars_aux.reserve(vars.size());
 
-        k_vectors.emplace_back(ode(t, vars));
+        kvec.emplace_back(ode(t, vars));
         for (int8 i = 1; i < m_tableau.stage(); i++)
         {
             for (std::size_t j = 0; j < vars.size(); j++)
             {
                 double k_sum = 0.0;
                 for (int8 k = 0; k < i; k++)
-                    k_sum += m_tableau.beta()[i - 1][k] * k_vectors[k][j];
+                    k_sum += m_tableau.beta()[i - 1][k] * kvec[k][j];
                 vars_aux[j] = vars[j] + k_sum * dt;
             }
-            k_vectors[i] = ode(t, vars_aux);
+            kvec[i] = ode(t, vars_aux);
         }
-        return k_vectors;
+        return kvec;
     }
 
     integrator::vector1d integrator::generate_solution(double dt,
-                                                       const vector2d &k_vectors,
+                                                       const vector2d &kvec,
                                                        const vector1d &vars,
                                                        const vector1d &coefs) const
     {
@@ -52,10 +52,21 @@ namespace rk
         {
             double sum = 0.0;
             for (int8 i = 0; i < m_tableau.stage(); i++)
-                sum += coefs[i] * k_vectors[i][j];
+                sum += coefs[i] * kvec[i][j];
             DBG_LOG_IF(isnan(sum), "NaN encountered when computing runge-kutta solution")
             sol[j] = vars[j] + sum * dt;
         }
+        return sol;
+    }
+
+    integrator::vector1d integrator::forward(double &t,
+                                             double dt,
+                                             const vector1d &vars,
+                                             vector1d (*ode)(double, const vector1d &)) const
+    {
+        const vector2d kvec = k_vectors(t, dt, vars, ode);
+        const vector1d sol = generate_solution(dt, kvec, vars, m_tableau.coefs());
+        t += dt;
         return sol;
     }
 }
