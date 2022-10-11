4582154d8c7466467ed1373c5a22f7e14b65c87e
Ismael Bueno <ismaelwno@gmail.com>
2022-10-11 23:46:45 +0200

    Implemented templated k_vectors and added the declaration of generate_sol function

--- runge-kutta/include/rungekutta.hpp
+++ runge-kutta/include/rungekutta.hpp
@@ -35,7 +35,38 @@ namespace rk
                            const vector1d &vars,
                            T &params,
                            vector1d (*ode)(double, const vector1d &, T &)) const;
+
+        vector1d generate_solution(double dt,
+                                   const vector2d &k_vectors,
+                                   const vector1d &vars,
+                                   const vector1d &coefs) const;
     };
+
+    template <typename T>
+    integrator::vector2d integrator::k_vectors(double t,
+                                               double dt,
+                                               const vector1d &vars,
+                                               T &params,
+                                               vector1d (*ode)(double, const vector1d &, T &)) const
+    {
+        vector2d k_vectors = reserve(m_tableau.stage(), vars.size());
+        vector1d vars_aux;
+        vars_aux.reserve(vars.size());
+
+        k_vectors.emplace_back(ode(t, vars, params));
+        for (int8 i = 1; i < m_tableau.stage(); i++)
+        {
+            for (std::size_t j = 0; j < vars.size(); j++)
+            {
+                double k_sum = 0.0;
+                for (int8 k = 0; k < i; k++)
+                    k_sum += m_tableau.beta()[i - 1][k] * k_vectors[k][j];
+                vars_aux[j] = vars[j] + k_sum * dt;
+            }
+            k_vectors[i] = ode(t, vars_aux, params);
+        }
+        return k_vectors;
+    }
 }
 
 #endif
