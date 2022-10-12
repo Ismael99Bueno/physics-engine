1a468d0233d746710b1e32621f5e26546af8d86c
Ismael Bueno <ismaelwno@gmail.com>
2022-10-12 22:57:09 +0200

    Changed alias from int to uint

--- runge-kutta/src/rk_tableau.cpp
+++ runge-kutta/src/rk_tableau.cpp
@@ -5,50 +5,50 @@ namespace rk
     tableau::tableau(const vector1d &alpha,
                      const vector2d &beta,
                      const vector1d &coefs,
-                     int8 stage,
-                     int8 order) : m_alpha(alpha),
-                                   m_coefs1(coefs),
-                                   m_beta(beta),
-                                   m_embedded(false),
-                                   m_stage(stage),
-                                   m_order(order) {}
+                     uint8 stage,
+                     uint8 order) : m_alpha(alpha),
+                                    m_coefs1(coefs),
+                                    m_beta(beta),
+                                    m_embedded(false),
+                                    m_stage(stage),
+                                    m_order(order) {}
 
     tableau::tableau(const vector1d &alpha,
                      const vector2d &beta,
                      const vector1d &coefs1,
                      const vector1d &coefs2,
-                     int8 stage,
-                     int8 order) : m_alpha(alpha),
-                                   m_coefs1(coefs1),
-                                   m_coefs2(coefs1),
-                                   m_beta(beta),
-                                   m_embedded(true),
-                                   m_stage(stage),
-                                   m_order(order) {}
+                     uint8 stage,
+                     uint8 order) : m_alpha(alpha),
+                                    m_coefs1(coefs1),
+                                    m_coefs2(coefs1),
+                                    m_beta(beta),
+                                    m_embedded(true),
+                                    m_stage(stage),
+                                    m_order(order) {}
 
     tableau::tableau(vector1d &&alpha,
                      vector2d &&beta,
                      vector1d &&coefs,
-                     int8 stage,
-                     int8 order) : m_alpha(std::move(alpha)),
-                                   m_coefs1(std::move(coefs)),
-                                   m_beta(std::move(beta)),
-                                   m_embedded(false),
-                                   m_stage(stage),
-                                   m_order(order) {}
+                     uint8 stage,
+                     uint8 order) : m_alpha(std::move(alpha)),
+                                    m_coefs1(std::move(coefs)),
+                                    m_beta(std::move(beta)),
+                                    m_embedded(false),
+                                    m_stage(stage),
+                                    m_order(order) {}
 
     tableau::tableau(vector1d &&alpha,
                      vector2d &&beta,
                      vector1d &&coefs1,
                      vector1d &&coefs2,
-                     int8 stage,
-                     int8 order) : m_alpha(std::move(alpha)),
-                                   m_coefs1(std::move(coefs1)),
-                                   m_coefs2(std::move(coefs2)),
-                                   m_beta(std::move(beta)),
-                                   m_embedded(true),
-                                   m_stage(stage),
-                                   m_order(order) {}
+                     uint8 stage,
+                     uint8 order) : m_alpha(std::move(alpha)),
+                                    m_coefs1(std::move(coefs1)),
+                                    m_coefs2(std::move(coefs2)),
+                                    m_beta(std::move(beta)),
+                                    m_embedded(true),
+                                    m_stage(stage),
+                                    m_order(order) {}
 
     const tableau::vector1d &tableau::alpha() const { return m_alpha; }
     const tableau::vector2d &tableau::beta() const { return m_beta; }
@@ -61,7 +61,7 @@ namespace rk
         return m_coefs2;
     }
     bool tableau::embedded() const { return m_embedded; }
-    tableau::int8 tableau::stage() const { return m_stage; }
-    tableau::int8 tableau::order() const { return m_order; }
+    tableau::uint8 tableau::stage() const { return m_stage; }
+    tableau::uint8 tableau::order() const { return m_order; }
 
 }
