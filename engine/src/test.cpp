6d0bdfd6bdf5103a276e008107d9b24943b251ef
Ismael Bueno <ismaelwno@gmail.com>
2022-10-12 22:33:43 +0200

    Implemented some initial tests

--- engine/src/test.cpp
+++ engine/src/test.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include "rk_integrator.hpp"
+#include "rk_prefab.hpp"
+
+std::vector<double> ode(double t, const std::vector<double> &vars)
+{
+    return {vars[1], -vars[0]};
+}
 
 int main()
 {
-    std::cout << "Hello Test\n";
+    rk::integrator integ(rk::rk4);
+    std::vector<double> vars = {1.0, 0.0};
+    double t = 0.0;
+
+    while (t < 10.0)
+    {
+        vars = integ.forward(t, 0.01, vars, ode);
+        std::cout << "t: " << t << " x: " << vars[0] << std::endl;
+    }
 }
