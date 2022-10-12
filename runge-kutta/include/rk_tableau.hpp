1be28c5ec7c79f305535ed22a1af3014107167b8
Ismael Bueno <ismaelwno@gmail.com>
2022-10-12 22:56:43 +0200

    Changed alias from int8 to uint8

--- runge-kutta/include/rk_tableau.hpp
+++ runge-kutta/include/rk_tableau.hpp
@@ -11,7 +11,7 @@ namespace rk
     private:
         using vector1d = std::vector<double>;
         using vector2d = std::vector<std::vector<double>>;
-        using int8 = std::uint8_t;
+        using uint8 = std::uint8_t;
 
     public:
         tableau() = delete;
@@ -19,28 +19,28 @@ namespace rk
         tableau(const vector1d &alpha,
                 const vector2d &beta,
                 const vector1d &coefs,
-                int8 stage,
-                int8 order);
+                uint8 stage,
+                uint8 order);
 
         tableau(const vector1d &alpha,
                 const vector2d &beta,
                 const vector1d &coefs1,
                 const vector1d &coefs2,
-                int8 stage,
-                int8 order);
+                uint8 stage,
+                uint8 order);
 
         tableau(vector1d &&alpha,
                 vector2d &&beta,
                 vector1d &&coefs,
-                int8 stage,
-                int8 order);
+                uint8 stage,
+                uint8 order);
 
         tableau(vector1d &&alpha,
                 vector2d &&beta,
                 vector1d &&coefs1,
                 vector1d &&coefs2,
-                int8 stage,
-                int8 order);
+                uint8 stage,
+                uint8 order);
 
         const vector1d &alpha() const;
         const vector2d &beta() const;
@@ -48,14 +48,14 @@ namespace rk
         const vector1d &coefs1() const;
         const vector1d &coefs2() const;
         bool embedded() const;
-        int8 stage() const;
-        int8 order() const;
+        uint8 stage() const;
+        uint8 order() const;
 
     private:
         vector1d m_alpha, m_coefs1, m_coefs2;
         vector2d m_beta;
         bool m_embedded;
-        int8 m_stage, m_order;
+        uint8 m_stage, m_order;
     };
 }
 
