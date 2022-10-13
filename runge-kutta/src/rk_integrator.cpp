d56b81152da3861c01230004b66485827f25e537
Ismael Bueno <ismaelwno@gmail.com>
2022-10-13 19:58:43 +0200

    Implemented new forward methods and other useful getters and setters

--- runge-kutta/src/rk_integrator.cpp
+++ runge-kutta/src/rk_integrator.cpp
@@ -2,23 +2,30 @@
 #include "debug.h"
 #include <cmath>
 
+#define SAFETY_FACTOR 0.85
+#define TOL_PART 256.0
+
 namespace rk
 {
     integrator::integrator(const tableau &tb,
-                           const double tolerance = 1e-6,
-                           const double min_dt = 1.e-6,
-                           const double max_dt = 1.0) : m_tableau(tb),
-                                                        m_tolerance(tolerance),
-                                                        m_min_dt(min_dt),
-                                                        m_max_dt(max_dt) {}
+                           const double tolerance,
+                           const double min_dt,
+                           const double max_dt) : m_tableau(tb),
+                                                  m_tolerance(tolerance),
+                                                  m_min_dt(min_dt),
+                                                  m_max_dt(max_dt),
+                                                  m_error(0.0),
+                                                  m_valid(true) {}
 
     integrator::integrator(tableau &&tb,
-                           const double tolerance = 1e-6,
-                           const double min_dt = 1.e-6,
-                           const double max_dt = 1.0) : m_tableau(std::move(tb)),
-                                                        m_tolerance(tolerance),
-                                                        m_min_dt(min_dt),
-                                                        m_max_dt(max_dt) {}
+                           const double tolerance,
+                           const double min_dt,
+                           const double max_dt) : m_tableau(std::move(tb)),
+                                                  m_tolerance(tolerance),
+                                                  m_min_dt(min_dt),
+                                                  m_max_dt(max_dt),
+                                                  m_error(0.0),
+                                                  m_valid(true) {}
 
     integrator::vector2d integrator::reserve(const uint32 n, const uint32 m)
     {
@@ -64,7 +71,8 @@ namespace rk
             double sum = 0.0;
             for (uint8 i = 0; i < m_tableau.stage(); i++)
                 sum += coefs[i] * kvec[i][j];
-            DBG_LOG_IF(isnan(sum), "NaN encountered when computing runge-kutta solution")
+            m_valid &= !isnan(sum);
+            DBG_LOG_IF(!m_valid, "NaN encountered when computing runge-kutta solution")
             sol.emplace_back(vars[j] + sum * dt);
         }
         return sol;
@@ -85,6 +93,7 @@ namespace rk
                                                  const vector1d &vars,
                                                  vector1d (*ode)(double, const vector1d &)) const
     {
+        m_valid = true;
         const vector2d kvec = k_vectors(t, dt, vars, ode);
         const vector1d sol = generate_solution(dt, kvec, vars, m_tableau.coefs());
         t += dt;
@@ -97,14 +106,24 @@ namespace rk
                                                          vector1d (*ode)(double, const vector1d &)) const
     {
         DBG_LOG_IF(m_tableau.embedded(), "Table has an embedded solution. Use an embedded adaptive method for better efficiency")
-        vector1d sol, aux_sol;
-        double error;
-        do
+        vector1d sol;
+        for (;;)
         {
-        } while (error > m_tolerance);
+            const vector1d aux_sol = integrate(t, dt, vars, m_tableau.coefs(), ode);
+            sol = integrate(t, dt / 2.0, vars, m_tableau.coefs(), ode);
+            sol = integrate(t, dt / 2.0, sol, m_tableau.coefs(), ode);
+            m_error = reiterative_error(sol, aux_sol);
+            if (dt_off_bounds(dt) || m_error <= m_tolerance)
+                break;
+            dt /= 2.0;
+        }
+        m_error = std::max(m_error, m_tolerance / TOL_PART);
+        t += dt;
+        dt = std::clamp(SAFETY_FACTOR * dt * std::pow(m_tolerance / m_error, 1.0 / m_tableau.order()), m_min_dt, m_max_dt);
+        return sol;
     }
 
-    std::uint32_t ipow(std::uint32_t base, std::uint32_t exponent)
+    static std::uint32_t ipow(std::uint32_t base, std::uint32_t exponent)
     {
         int result = 1;
         for (;;)
@@ -120,6 +139,8 @@ namespace rk
         return result;
     }
 
+    bool integrator::dt_off_bounds(const double dt) const { return dt <= m_min_dt || dt >= m_max_dt; }
+
     double integrator::embedded_error(const vector1d &sol1, const vector1d &sol2)
     {
         double result = 0.0;
@@ -133,4 +154,14 @@ namespace rk
         const uint32 coeff = ipow(2, m_tableau.order()) - 1;
         return embedded_error(sol1, sol2) / coeff;
     }
+
+    double integrator::tolerance() const { return m_tolerance; }
+    double integrator::min_dt() const { return m_min_dt; }
+    double integrator::max_dt() const { return m_max_dt; }
+    double integrator::error() const { return m_error; }
+    bool integrator::valid() const { return m_valid; }
+
+    void integrator::tolerance(const double val) { m_tolerance = val; }
+    void integrator::min_dt(const double val) { m_min_dt = val; }
+    void integrator::max_dt(const double val) { m_max_dt = val; }
 }
