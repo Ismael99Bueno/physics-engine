16c2f3d7334666f216df4582568feb7c824cc7e0
Ismael Bueno <ismaelwno@gmail.com>
2022-10-15 01:08:15 +0200

    Updated test

--- engine/src/test.cpp
+++ engine/src/test.cpp
@@ -5,22 +5,27 @@
 
 std::vector<double> ode(double t, const std::vector<double> &vars)
 {
-    return {vars[1], -vars[0]};
+    double x = vars[0], y = vars[1];
+    return {x * (1.0 - y), y * (x - 1.0)};
 }
 
 int main()
 {
-    std::vector<double> vars = {1.0, 0.0};
-    rk::integrator integ(rk::rkf78, vars);
+    std::vector<double> vars = {4.0, 10.0};
+    rk::integrator integ(rk::rkf78, vars, 1e-9);
 
     double t = 0.0, dt = 0.001;
+    const double tf = 100.0;
 
     std::size_t iters = 0;
-    while (t + dt < 10.0)
+    while (t + dt < tf)
     {
-        integ.raw_forward(t, dt, ode);
-        std::cout << "t: " << t << " x: " << vars[0] << " error: " << integ.error() << "\n";
+        integ.embedded_forward(t, dt, ode);
+        std::cout << "t: " << t << " x: " << vars[0] << " y: " << vars[1] << " error: " << integ.error() << "\n";
         iters++;
     }
+    dt = tf - t;
+    integ.raw_forward(t, dt, ode);
+    std::cout << "t: " << t << " x: " << vars[0] << " y: " << vars[1] << " error: " << integ.error() << "\n";
     std::cout << iters << "\n";
 }
