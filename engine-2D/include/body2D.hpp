e827f642ae8c1b1c71409c1d26dd319c4857750f
Ismael Bueno <ismaelwno@gmail.com>
2022-10-15 22:02:49 +0200

    Added setters for all member variables

--- engine-2D/include/body2D.hpp
+++ engine-2D/include/body2D.hpp
@@ -15,12 +15,17 @@ namespace physics
         body2D(const vec2 &pos, const vec2 &vel, double mass);
         body2D(const vec2 &pos, const vec2 &vel, double mass, double charge);
 
-        vec2 &position();
-        const vec2 &position() const;
+        vec2 &pos();
         vec2 &vel();
+        const vec2 &pos() const;
         const vec2 &vel() const;
         double mass() const;
         double charge() const;
+
+        void pos(const vec2 &pos);
+        void vel(const vec2 &vel);
+        void mass(double mass);
+        void charge(double charge);
 
     private:
         vec2 m_pos = {0.0, 0.0}, m_vel = {0.0, 0.0};
