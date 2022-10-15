f3bd916f5fa67aa3bb10b1a05a809797243f5c80
Ismael Bueno <ismaelwno@gmail.com>
2022-10-15 19:47:05 +0200

    Tested rk

--- engine/src/test.cpp
+++ engine/src/test.cpp
@@ -17,16 +17,16 @@ std::vector<double> ode(double t, const std::vector<double> &vars, const lv &par
 int main()
 {
     std::vector<double> vars = {4.0, 10.0};
-    rk::integrator integ(rk::rkf78, vars, 1e-12);
+    rk::integrator integ(rk::rk38, vars, 1e-12);
     lv prm = {2.0, 1.0, 1.0, 1.0};
 
     double t = 0.0, dt = 0.001;
-    const double tf = 100.0;
+    const double tf = 10.0;
 
     std::size_t iters = 0;
     while (t + dt < tf)
     {
-        integ.embedded_forward(t, dt, prm, ode);
+        integ.reiterative_forward(t, dt, prm, ode);
         std::cout << "t: " << t << " x: " << vars[0] << " y: " << vars[1] << " error: " << integ.error() << "\n";
         iters++;
     }
