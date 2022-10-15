6688d368352b337a436622858d3a180734c77f43
Ismael Bueno <ismaelwno@gmail.com>
2022-10-15 15:37:33 +0200

    Updated the test accordingly

--- engine/src/test.cpp
+++ engine/src/test.cpp
@@ -3,16 +3,22 @@
 #include "rk_prefab.hpp"
 #include <cmath>
 
-std::vector<double> ode(double t, const std::vector<double> &vars)
+struct lv
+{
+    double alpha, beta, gamma, delta;
+};
+
+std::vector<double> ode(double t, const std::vector<double> &vars, const lv &params)
 {
     double x = vars[0], y = vars[1];
-    return {x * (1.0 - y), y * (x - 1.0)};
+    return {x * (params.alpha - params.beta * y), y * (params.gamma * x - params.delta)};
 }
 
 int main()
 {
     std::vector<double> vars = {4.0, 10.0};
-    rk::integrator integ(rk::rkf78, vars, 1e-9);
+    rk::integrator integ(rk::rkf78, vars, 1e-12);
+    lv prm = {2.0, 1.0, 1.0, 1.0};
 
     double t = 0.0, dt = 0.001;
     const double tf = 100.0;
@@ -20,12 +26,12 @@ int main()
     std::size_t iters = 0;
     while (t + dt < tf)
     {
-        integ.embedded_forward(t, dt, ode);
+        integ.embedded_forward(t, dt, prm, ode);
         std::cout << "t: " << t << " x: " << vars[0] << " y: " << vars[1] << " error: " << integ.error() << "\n";
         iters++;
     }
     dt = tf - t;
-    integ.raw_forward(t, dt, ode);
+    integ.raw_forward(t, dt, prm, ode);
     std::cout << "t: " << t << " x: " << vars[0] << " y: " << vars[1] << " error: " << integ.error() << "\n";
     std::cout << iters << "\n";
 }
