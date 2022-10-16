f12a9a159379bdbd42d3428e65f5a3f0d82d9a07
Ismael Bueno <ismaelwno@gmail.com>
2022-10-16 20:20:34 +0200

    Changed add functionality and incorporated forces to the general ode

--- engine-2D/include/engine2D.hpp
+++ engine-2D/include/engine2D.hpp
@@ -3,6 +3,8 @@
 
 #include "integrator.hpp"
 #include "entity2D.hpp"
+#include "force2D.hpp"
+#include <unordered_set>
 #include <cstdint>
 #include <vector>
 
@@ -16,17 +18,19 @@ namespace physics
                  double dt = 0.001,
                  std::uint32_t allocations = 1000);
 
-        void add(std::size_t amount = 1);
-        void add(const entity2D &entity);
-        void add(const entity2D &entity, std::size_t amount);
-        void add(const std::vector<entity2D> &entities);
+        entity2D &add(const entity2D &&entity = entity2D()); // CHECK DEFAULT!! Maybe it does not initialize to zero
+        void add(const force2D &force);
 
         bool raw_forward();
         void until(double time_threshold, bool (engine2D::*forward)());
 
+        double elapsed() const;
+
     private:
         std::vector<entity2D> m_entities;
         std::vector<double> m_buffer;
+        std::unordered_set<const force2D *> m_forces;
+
         rk::integrator m_integ;
         double m_dt, m_t = 0.0;
 
--- engine-2D/src/engine2D.cpp
+++ engine-2D/src/engine2D.cpp
@@ -3,14 +3,31 @@
 
 namespace physics
 {
+    vec2 apply_force(const entity2D &entity, const force2D &force)
+    {
+        const body2D &ebody = entity.body();
+        const vec2 &pos = ebody.pos(), vel = ebody.pos();
+        const body2D body = {pos, vel, ebody.mass(), ebody.charge()};
+        return force.acceleration(body);
+    }
+
     std::vector<double> ode(const double t, const std::vector<double> &state, const engine2D &eng)
     {
         std::vector<double> result(state.size(), 0.0);
         for (std::size_t i = 0; i < eng.m_entities.size(); i++)
         {
+            const entity2D &entity = eng.m_entities[i];
             std::size_t j = 4 * i;
             result[j + 0] = state[j + 2];
             result[j + 1] = state[j + 3];
+            for (const force2D *force : eng.m_forces)
+            {
+                if (!force->contains(entity))
+                    continue;
+                vec2 accel = apply_force(entity, *force);
+                result[j + 2] += accel.x;
+                result[j + 3] += accel.y;
+            }
         }
         return result;
     }
@@ -23,14 +40,7 @@ namespace physics
         m_integ.reserve_state(4 * allocations);
     }
 
-    void engine2D::add(const std::size_t amount)
-    {
-        DBG_LOG_IF(amount < 1, "Adding 0 entities...\n")
-        for (std::size_t i = 0; i < amount; i++)
-            add(entity2D());
-    }
-
-    void engine2D::add(const entity2D &entity)
+    entity2D &engine2D::add(const entity2D &&entity)
     {
         m_buffer.emplace_back(entity.body().pos().x);
         m_buffer.emplace_back(entity.body().pos().y);
@@ -39,20 +49,10 @@ namespace physics
         m_entities.emplace_back(entity);
         m_integ.resize_state(m_buffer.size());
         m_entities[m_entities.size() - 1].m_buffer = {&m_buffer, m_buffer.size() - 4};
+        return m_entities[m_entities.size() - 1];
     }
 
-    void engine2D::add(const entity2D &entity, const std::size_t amount)
-    {
-        DBG_LOG_IF(amount < 1, "Adding 0 entities...\n")
-        for (std::size_t i = 0; i < amount; i++)
-            add(entity);
-    }
-
-    void engine2D::add(const std::vector<entity2D> &entities)
-    {
-        for (const entity2D &entity : entities)
-            add(entity);
-    }
+    void engine2D::add(const force2D &force) { m_forces.insert(&force); }
 
     bool engine2D::raw_forward()
     {
@@ -66,4 +66,6 @@ namespace physics
         while (m_t < time_threshold)
             (this->*forward)();
     }
+
+    double engine2D::elapsed() const { return m_t; }
 }
