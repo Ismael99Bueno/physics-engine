0e08e31e4aaafccef55921950daf8ca043e13319
Ismael Bueno <ismaelwno@gmail.com>
2022-10-16 21:07:42 +0200

    Tested interactions

--- playground/src/test.cpp
+++ playground/src/test.cpp
@@ -4,6 +4,7 @@
 #include "vec_ptr.hpp"
 #include "vec2.hpp"
 #include "force2D.hpp"
+#include "interaction2D.hpp"
 #include <cmath>
 
 using namespace physics;
@@ -16,18 +17,31 @@ class gravity : public force2D
     }
 };
 
+class gravitation : public interaction2D
+{
+    vec2 acceleration(const body2D &b1, const body2D &b2) const override
+    {
+        return (b2.pos() - b1.pos()).normalized() * (b2.mass() / b1.pos().sq_dist(b2.pos()));
+    }
+};
+
 int main()
 {
     engine2D eng(rk::rk4);
+    entity2D &e1 = eng.add(), &e2 = eng.add();
+    e1.body().pos({1.0, 0.0});
+    e1.body().vel({0.0, -1.0});
+    e1.dispatch();
 
-    gravity grav;
-    entity2D &e = eng.add();
-    grav.add(e);
+    gravitation grav;
+
+    grav.add(e1);
+    grav.add(e2);
     eng.add(grav);
     while (eng.elapsed() < 1.0)
     {
         eng.raw_forward();
-        e.retrieve();
-        std::cout << e.body().pos() << " " << e.body().vel() << "\n";
+        e1.retrieve();
+        std::cout << e1.body().pos() << " " << e1.body().vel() << "\n";
     }
 }
