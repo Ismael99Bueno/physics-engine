1de25ac57c255c8d9c9b06d9ba9cabe43333f7ea
Ismael Bueno <ismaelwno@gmail.com>
2022-10-16 20:12:32 +0200

    Added functionality to add, remove and check if an entity is contained in a force

--- engine-2D/include/force2D.hpp
+++ engine-2D/include/force2D.hpp
@@ -10,11 +10,18 @@ namespace physics
     class force2D
     {
     public:
-        force2D(std::size_t reserve = 10);
+        force2D(std::size_t allocations = 10);
         virtual vec2 acceleration(const body2D &body) const = 0;
 
+        void add(const entity2D &entity);
+        void remove(const entity2D &entity);
+        bool contains(const entity2D &entity) const;
+
+        std::unordered_set<const entity2D *> &entities();
+        const std::unordered_set<const entity2D *> &entities() const;
+
     private:
-        std::unordered_set<entity2D *> entities;
+        std::unordered_set<const entity2D *> m_entities;
     };
 }
 
--- engine-2D/src/force2D.cpp
+++ engine-2D/src/force2D.cpp
@@ -2,5 +2,12 @@
 
 namespace physics
 {
-    force2D::force2D(const std::size_t reserve) { entities.reserve(reserve); }
+    force2D::force2D(const std::size_t allocations) { m_entities.reserve(allocations); }
+
+    void force2D::add(const entity2D &entity) { m_entities.insert(&entity); }
+    void force2D::remove(const entity2D &entity) { m_entities.erase(&entity); }
+    bool force2D::contains(const entity2D &entity) const { return m_entities.find(&entity) != m_entities.end(); }
+
+    std::unordered_set<const entity2D *> &force2D::entities() { return m_entities; }
+    const std::unordered_set<const entity2D *> &force2D::entities() const { return m_entities; }
 }
