75b200f5a3692b566e636eb83640b4dad8e5aef2
Ismael Bueno <ismaelwno@gmail.com>
2022-10-16 20:27:52 +0200

    Updated test

--- playground/src/test.cpp
+++ playground/src/test.cpp
@@ -2,15 +2,32 @@
 #include "prefab.hpp"
 #include "engine2D.hpp"
 #include "vec_ptr.hpp"
+#include "vec2.hpp"
+#include "force2D.hpp"
 #include <cmath>
 
 using namespace physics;
 
+class gravity : public force2D
+{
+    vec2 acceleration(const body2D &body) const override
+    {
+        return vec2(0.0, -1.0);
+    }
+};
+
 int main()
 {
     engine2D eng(rk::rk4);
-    entity2D e1 = body2D({1.0, 2.0}, {3.0, -1.0}), e2 = body2D({-1.0, 2.0}, {-3.0, 1.2});
-    eng.add();
-    eng.add({e1, e2});
-    eng.until(10.0, &engine2D::raw_forward);
+
+    gravity grav;
+    entity2D &e = eng.add();
+    grav.add(e);
+    eng.add(grav);
+    while (eng.elapsed() < 1.0)
+    {
+        eng.raw_forward();
+        e.retrieve();
+        std::cout << e.body().pos() << " " << e.body().vel() << "\n";
+    }
 }
