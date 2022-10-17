e9955c2b34fe8dd7717da39d37ad187c406e5607
Ismael Bueno <ismaelwno@gmail.com>
2022-10-17 22:18:20 +0200

    Tweaked with SFML

--- playground/src/main.cpp
+++ playground/src/main.cpp
@@ -1,10 +1,15 @@
 #include <SFML/Graphics.hpp>
 
+#define WIDTH 1280.0
+#define HEIGHT 720.0
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    window.setView(sf::View(sf::Vector2f(0.f, 0.f), sf::Vector2f(WIDTH, -HEIGHT)));
+    shape.setOrigin(100.f, 100.f);
 
     while (window.isOpen())
     {
